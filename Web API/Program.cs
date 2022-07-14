1523c7412cc7c00a52674a79b261075c43f3449c
dzenitAl <dzenita.alibegic@edu.fit.ba>
2022-07-14 21:04:03 +0200

    Added SetupService class for Automatic Migration

--- Web API/Program.cs
+++ Web API/Program.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLayer.EF;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
 
 namespace Web_API
 {
@@ -13,7 +15,14 @@ namespace Web_API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var database = scope.ServiceProvider.GetService<TaskTrackerDbContext>();
+                new SetupService().Init(database);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Web API/SetupService.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_API
+{
+    public class SetupService
+    {
+        public void Init(TaskTrackerDbContext context)
+        {
+            context.Database.Migrate();
+        }
+    }
+}
